b5b96dc49f551a6990c71fe86bcc094efa2623a5
winspace2050 <raphael.pele86@gmail.com>
2026-05-24 13:09:36 +0200

    Amélioration anti-XSS
    Echappement des apostrophes

--- web/js/utils.js
+++ web/js/utils.js
@@ -63,7 +63,7 @@ function escapeHtml(str) {
         .replace(/&/g, "&amp;")
         .replace(/</g, "&lt;")
         .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;");
+        .replace(/"/g, "&quot;")
         .replace(/'/g, "&#x27;");
 }
 
